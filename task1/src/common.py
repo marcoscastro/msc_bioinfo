31b2963db4f3c9b537c71543f78b174a563cb95b
marcos <marcos@marcos-not.(none)>
2015-03-23 06:22:24 -0300

    added module common.py that contains common functions

---
+++ task1/src/common.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+'''
+	This file implements common functions
+'''
+
+# returns the reverse-complement of a DNA sequence
+def reverse_complement(dna_sequence):
+	dict_bases = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+	reverse_dna = dna_sequence.upper()[::-1]
+	return ''.join([dict_bases[base] for base in reverse_dna])
+
+
+# tests
+if __name__ == "__main__":
+
+	answer = 'ATAGCGCTCTAGATAGCTAGTCAGT'
+	if reverse_complement('ACTGACTAGCTATCTAGAGCGCTAT') == answer:
+		print('Success in the reverse_complement')
+	else:
+		print('Fail in the reverse_complement')
