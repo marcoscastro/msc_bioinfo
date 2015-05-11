015ae2248a5294aee6d1a1c77ecaacddfcbf9d21
marcos <marcos@marcos-not.(none)>
2015-05-10 22:13:04 -0300

    rename folder

---
+++ blast/blast2sequences.py
@@ -0,0 +1,14 @@
+# encoding:utf-8
+
+'''
+	Implementation of Blast2Sequences Algorithm
+	Author: Marcos Castro
+'''
+
+def blast2sequences():
+	# TODO
+	pass
+
+
+if __name__ == "__main__":
+	print('TODO...')
