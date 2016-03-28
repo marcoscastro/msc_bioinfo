8fac16c8ded55f5214f284b8d252a159d7d3f435
marcoscastro <mcastrosouza@live.com>
2016-03-28 17:20:59 -0300

    update documentation

--- detection_genetic_variations/src/fastq_reader.h
+++ detection_genetic_variations/src/fastq_reader.h
@@ -1,4 +1,8 @@
 #ifndef FASTQ_READER_H
 #define FASTQ_READER_H
 
+/*! \brief Functions for handling of FASTQ files.
+ *
+ */
+
 #endif
--- detection_genetic_variations/src/main.cpp
+++ detection_genetic_variations/src/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 
-/**
- * This file contains the main function
- */
-
 int main(int argc, char *argv[])
 {
 	std::cout << "hello ACTG" << std::endl;
