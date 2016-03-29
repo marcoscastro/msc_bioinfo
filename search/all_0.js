748855a25c95efd682f65f7a40c01a7696f09bf3
marcoscastro <mcastrosouza@live.com>
2016-03-29 00:43:10 -0300

    update github pages

---
+++ search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['fastq_5freader_2eh',['fastq_reader.h',['../fastq__reader_8h.html',1,'']]],
+  ['fastqreader',['FASTQReader',['../class_f_a_s_t_q_reader.html',1,'FASTQReader'],['../class_f_a_s_t_q_reader.html#a794202383b7c9a5ed978bf6e27a86248',1,'FASTQReader::FASTQReader()']]]
+];
---
+++ search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['fastqreader',['FASTQReader',['../class_f_a_s_t_q_reader.html#a794202383b7c9a5ed978bf6e27a86248',1,'FASTQReader']]]
+];
