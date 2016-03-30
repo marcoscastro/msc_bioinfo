fb75b33108bdd2c8f3a49201da9d2140d5539553
marcoscastro <mcastrosouza@live.com>
2016-03-30 17:13:03 -0300

    update main file

--- detection_genetic_variations/src/main.cpp
+++ detection_genetic_variations/src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include "fastq_reader.h"
 
+/*! \mainpage Main Page of the project
+ *
+ * \section intro_sec Introduction
+ *
+ * Project: De Bruijn Graphs for detection of genetic variations by GBS (Genotyping By Sequencing).
+ *
+ * The project's code is hosted in: https://github.com/marcoscastro/msc_bioinfo
+ *
+ * The application is being made in C++ language.
+ * 
+ * News coming soon...
+ *
+ * \section install_sec Installation
+ *  
+ * In soon... Keep calm!
+ */
+
 /*! \brief Main function of the application.
  *	\author Marcos Castro
  *	\copyright GNU Public License.
