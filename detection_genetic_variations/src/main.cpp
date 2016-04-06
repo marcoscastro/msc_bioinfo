d760f57595ffa3a0c68968b4ba7d3d2483d731f4
marcoscastro <mcastrosouza@live.com>
2016-04-05 22:40:58 -0300

    test modeling

--- detection_genetic_variations/src/main.cpp
+++ detection_genetic_variations/src/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 		DeBruijnGraph graph(K, fastq_reader.getReads(),
 							fastq_reader.getTotalReads());
 
-		graph.showKMears();
+		//graph.showKMears();
 	}
 
 	return 0;
