2ab2aecea9fe56ebac4997785524bae52119c853
marcoscastro <mcastrosouza@live.com>
2016-04-13 15:12:33 -0300

    add comment in the kmer.cpp

--- detection_genetic_variations/src/kmer.cpp
+++ detection_genetic_variations/src/kmer.cpp
@@ -1,3 +1,11 @@
+/*! \file kmer.cpp
+ *	\brief Source-code of the class KMer.
+ *	\author Marcos Castro
+ *	\copyright GNU Public License.
+ *	\date 13/04/2016
+ *
+ */
+
 #include "kmer.h"
 
 KMer::KMer(std::string& sequence)
