f35186367928afd15c56ab654eae1fc10acb8e72
marcoscastro <mcastrosouza@live.com>
2016-05-11 13:51:39 -0300

    update documentatioN

--- search/all_3.js
+++ search/all_3.js
@@ -3,7 +3,7 @@ var searchData=
   ['getheader',['getHeader',['../class_read.html#a2a7c35a8da562a8e69a0aca95bf686da',1,'Read']]],
   ['getid',['getID',['../class_read.html#a1e1a37c865cde9046e379283605a510d',1,'Read']]],
   ['getqualityscore',['getQualityScore',['../class_read.html#aeaf12ee8ef28072450a53976c0a85208',1,'Read']]],
-  ['getreads',['getReads',['../class_f_a_s_t_q_reader.html#a019f4982e5e51b15e3f0a3e7ec85ce3e',1,'FASTQReader']]],
+  ['getreads',['getReads',['../class_f_a_s_t_q_reader.html#a019f4982e5e51b15e3f0a3e7ec85ce3e',1,'FASTQReader::getReads()'],['../class_k_mer.html#a18da44755ba93d132d4ae2690d064fe7',1,'KMer::getReads()']]],
   ['getsequence',['getSequence',['../class_k_mer.html#aa8396200cd9e179555cbf5a8037cebb0',1,'KMer::getSequence()'],['../class_node.html#a6e58c58e12ae52f54ae673ceb598f0fd',1,'Node::getSequence()'],['../class_read.html#a82ab15c3fb5712b0d2c003818039be03',1,'Read::getSequence()']]],
   ['gettotalreads',['getTotalReads',['../class_f_a_s_t_q_reader.html#a81256779e4288a37401daa36150b9d0d',1,'FASTQReader::getTotalReads()'],['../class_k_mer.html#a90d4bb7df6173b327a208f16d3f317d3',1,'KMer::getTotalReads()']]],
   ['gettotalsuccessors',['getTotalSuccessors',['../class_k_mer.html#adff9d75dc5fd5ef51ef7629cb897ef89',1,'KMer']]],
--- search/functions_9.js
+++ search/functions_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['setsuccessor',['setSuccessor',['../class_k_mer.html#a920e60ce3377d5217708b3425e9e2529',1,'KMer']]],
-  ['showkmers',['showKMers',['../class_de_bruijn_graph.html#ae87bce7cfda85eb941d598cbedf933c6',1,'DeBruijnGraph']]],
-  ['showread',['showRead',['../class_f_a_s_t_q_reader.html#a10863036d1fd4762daf873d93b7dd99f',1,'FASTQReader']]]
+  ['setsuccessor',['setSuccessor',['../class_k_mer.html#a4740edbea0c7765e25a7d47b88cfb5d3',1,'KMer']]],
+  ['showinfograph',['showInfoGraph',['../class_de_bruijn_graph.html#aa36a0ef811ebb1a9960c6b764bf42cde',1,'DeBruijnGraph']]],
+  ['showread',['showRead',['../class_f_a_s_t_q_reader.html#a10863036d1fd4762daf873d93b7dd99f',1,'FASTQReader']]],
+  ['showreadsshareskmersbyread',['showReadsSharesKMersByRead',['../class_de_bruijn_graph.html#acc19081c30eeb4883cbe070cb0902d8a',1,'DeBruijnGraph']]]
 ];
